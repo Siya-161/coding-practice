d6a5b330a2f69c6c830a17c84575312aa0415046
Siya-161 <104910960+Siya-161@users.noreply.github.com>
2025-02-02 10:21:09 +0200

    Update and rename Longest Word to Longest Word.java

--- easy/Longest Word.java
+++ easy/Longest Word.java
@@ -1,4 +1,5 @@
-//Question: Have the function LongestWord(sen) take the sen parameter being passed and return the longest word in the string. If there are two or more words that are the same length, return the first word from the string with that length. 
+//Question: Have the function LongestWord(sen) take the sen parameter being passed and return the longest word in the string. 
+//If there are two or more words that are the same length, return the first word from the string with that length. 
 //Ignore punctuation and assume sen will not be empty. Words may also contain numbers, for example "Hello world123 567"
 //Score: 7/10. 5 test case points (All the test case outputs were correct) and 2 time period points (This user submitted their solution in 20 minutes).
 //Runtime complexity: O(n).
