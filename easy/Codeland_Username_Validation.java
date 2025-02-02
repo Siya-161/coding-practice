4a88fe1334e77f0dd37e10e37968b5f70dde6a22
Siya-161 <104910960+Siya-161@users.noreply.github.com>
2025-02-02 15:24:00 +0200

    Update Codeland_Username_Validation.java

--- easy/Codeland_Username_Validation.java
+++ easy/Codeland_Username_Validation.java
@@ -1,4 +1,4 @@
-//Have the function CodelandUsernameValidation(str) take the str parameter being passed 
+//Challenge: Have the function CodelandUsernameValidation(str) take the str parameter being passed 
 //and determine if the string is a valid username according to the following rules:
 //1. The username is between 4 and 25 characters.
 //2. It must start with a letter.
